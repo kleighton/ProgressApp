13f5a43f80877bb17fada78c74f598bd42fb933b
Kyle Leighton <kleighton@processingpoint.com>
2017-10-10 19:21:25 -0700

    test commit to ProgressApp

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl(){
-  return !getQueryStringParameterByName('useMockApi') ? '/' : 'http://localhost:3001/';
+  return !getQueryStringParameterByName('useMockApi') ? 'https://obscure-falls-41633.herokuapp.com/' : '/';
 }
 
 function getQueryStringParameterByName(name, url){
