4533b1e62e7442864eed5458742fbefa00cf2108
Kyle Leighton <kleighton2@yahoo.com>
2017-12-26 20:43:29 -0800

    Added procfile

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -4,7 +4,7 @@ import open from 'open';
 import compression from 'compression';
 /*eslint-disable no-console*/
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 const app = express();
 
 app.use(compression());
