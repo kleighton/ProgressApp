e65dbb326626de9448f6a9067362746a5aaaeb42
Kyle Leighton <kleighton2@yahoo.com>
2018-01-10 21:04:50 -0800

    Adding favicon to distserver

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -2,6 +2,8 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import compression from 'compression';
+import favicon from 'serve-favicon';
+
 /*eslint-disable no-console*/
 
 const port = process.env.PORT || 4000;
@@ -9,6 +11,7 @@ const app = express();
 
 app.use(compression());
 app.use(express.static('dist'));
+app.use(favicon(path.join(__dirname , '../src/img/favicon.png')));
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'../dist/index.html'));
